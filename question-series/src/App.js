fe461c165bac5b729dea520d3f76d3ce3b59147b
Nitesh Pandey <112393577+nitesh-ui@users.noreply.github.com>
2022-12-22 20:07:25 +0530

    Add files via upload

--- question-series/src/App.js
+++ question-series/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Counter from './component/Counter';
 import Questions from './component/Questions';
@@ -8,15 +8,18 @@ function App() {
 
   const [counterValue, setCounterValue] = useState(0);
   const [counterValue1, setCounterValue1] = useState(1);
+  const [answers, setAnswers] = useState({});
 
   const incrementAllHandler = () => {
     setCounterValue(counterValue + 1);
     setCounterValue1(counterValue1 + 1);
   }
+  
+  console.log(answers);
 
   return (
     <div>
-      <Questions allQuestion={questions} selectedOption={questions[0]}/>
+      <Questions allQuestion={questions} selectedOption={questions[0]} answers={answers} setAnswers={setAnswers}/>
       <Counter value={counterValue} setCounterValue={setCounterValue} incrementAllHandler={incrementAllHandler}/>
       <Counter value={counterValue1} setCounterValue={setCounterValue1} incrementAllHandler={incrementAllHandler}/>
     </div>
