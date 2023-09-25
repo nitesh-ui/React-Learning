d79d77269a365a66bf87ac495e43100f7d9cc72c
Nitesh Pandey <112393577+nitesh-ui@users.noreply.github.com>
2023-09-25 18:54:18 +0530

    Update App.js

--- customHooks/App.js
+++ customHooks/App.js
@@ -1,17 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-import Counter from './Counter';
 import CounterOne from './components/counterOne';
 import CounterTwo from './components/counterTwo';
 
 function App() {
-
-  const [showCounter, setShowCounter] = useState(false);
-
   return (
     <div className="App">
-      <input type="checkbox" id="toggleCounter" checked={showCounter} onChange={({target}) => setShowCounter(target.checked)} />
-      <div>{showCounter && <Counter/>}</div>
       <CounterOne />
       <CounterTwo />
     </div>
